c7f5b0c80251a1a9b787c398adb254dc0006a8d8
Adrian Li <li.adrianmc@gmail.com>
2020-05-10 03:12:27 -0400

    small refactor

--- bin/funcs.js
+++ bin/funcs.js
@@ -7,70 +7,76 @@ const githubDownload = util.promisify(download);
 const deleteFile = util.promisify(fs.unlink);
 
 exports.processParams = async (projectLocation) => {
-    const projectPath = path.join(process.cwd(), projectLocation);
-    const projectName = projectLocation === "." ? path.basename(projectPath) : path.basename(projectLocation);
-    return { projectPath, projectName };
-}
+  const projectPath = path.join(process.cwd(), projectLocation);
+  const projectName =
+    projectLocation === "."
+      ? path.basename(projectPath)
+      : path.basename(projectLocation);
+  return { projectPath, projectName };
+};
 
-exports.validateParams = async(projectLocation) => {
-    // check that there is a project location provided
-    if (!projectLocation) {
-        console.log(`  Please specify a project directory:
+exports.validateParams = async (projectLocation) => {
+  // check that there is a project location provided
+  if (!projectLocation) {
+    console.log(`  Please specify a project directory:
     ${chalk.cyan("nano-react-app")} ${chalk.green("<project-directory>")}
 
   For example:
     ${chalk.cyan("nano-react-app")} ${chalk.green("my-react-app")}
     `);
-        return false;
-    }
-    return true;
-}
+    return false;
+  }
+  return true;
+};
 
 exports.createFolder = async (projectPath) => {
-    // create folder if it does not exist
-    const folderExists = fs.existsSync(projectPath);
-    if (!folderExists) {
-        process.stdout.write("  Creating directory...");
-        fs.mkdirSync(projectPath, { recursive: true });
-        process.stdout.write(chalk.green(" DONE\n"));
-    }
-}
+  // create folder if it does not exist
+  const folderExists = fs.existsSync(projectPath);
+  if (!folderExists) {
+    process.stdout.write("  Creating directory...");
+    fs.mkdirSync(projectPath, { recursive: true });
+    process.stdout.write(chalk.green(" DONE\n"));
+  }
+};
 
 exports.downloadTemplate = async (templateUrl, projectPath) => {
-    // download template project from github
-    process.stdout.write("  Downloading template project...");
-    await githubDownload(templateUrl, projectPath);
-    process.stdout.write(chalk.green(" DONE\n\n"));
-}
+  // download template project from github
+  process.stdout.write("  Downloading template project...");
+  await githubDownload(templateUrl, projectPath);
+  process.stdout.write(chalk.green(" DONE\n\n"));
+};
 
 exports.updateProjectFiles = async (projectPath, projectName) => {
+  // change package.json project name
+  const pkgJsonPath = path.join(projectPath, "package.json");
+  const packageJSON = JSON.parse(fs.readFileSync(pkgJsonPath, "utf8"));
+  const newPackageJSON = { ...packageJSON, name: projectName, description: "" };
 
-    // change package.json project name
-    const pkgJsonPath = path.join(projectPath, "package.json");
-    const packageJSON = JSON.parse(fs.readFileSync(pkgJsonPath, "utf8"));
-    const newPackageJSON = { ...packageJSON, name: projectName, description: "", };
+  fs.writeFileSync(
+    pkgJsonPath,
+    JSON.stringify(newPackageJSON, null, 2),
+    "utf8",
+  );
 
-    fs.writeFileSync(pkgJsonPath, JSON.stringify(newPackageJSON, null, 2), "utf8");
-
-    // delete unnecessary files
-    await deleteFile(path.join(projectPath, "LICENSE"));
-}
+  // delete unnecessary files
+  await deleteFile(path.join(projectPath, "LICENSE"));
+};
 
 exports.notifyUser = async (projectPath, projectName) => {
-    // notify user that the app is ready
-    console.log(`${chalk.bgMagenta(chalk.cyanBright("  SUCCESS!  "))}
+  // notify user that the app is ready
+  console.log(`${chalk.bgMagenta(chalk.cyanBright("  SUCCESS!  "))}
 
   Created project ${chalk.magenta(projectName)} at ${chalk.magenta(projectPath)}
   Navigate to that directory and run the following commands:
 
     1. ${chalk.cyan("npm install")} or ${chalk.cyan(
-        "yarn",
-    )} to install dependencies
+    "yarn",
+  )} to install dependencies
 
     2. ${chalk.cyan("npm start")} or ${chalk.cyan(
-        "yarn start",
-    )} to start developing
+    "yarn start",
+  )} to start developing
 
   ${chalk.yellow("Enjoy!")}
 `);
-}
+};
--- bin/nano-react-app.js
+++ bin/nano-react-app.js
@@ -1,26 +1,29 @@
 #!/usr/bin/env node
 const chalk = require("chalk");
-const funcs = require('./funcs');
+const funcs = require("./funcs");
 
-const jsxPath = 'nano-react-app/template-js';
-const tsxPath = 'nano-react-app/template-ts';
+const templates = {
+  js: `nano-react-app/template-js`,
+  ts: `nano-react-app/template-ts`,
+};
 
 const main = async () => {
+  console.log(`${chalk.bgMagenta(chalk.cyanBright("  NANO REACT APP  "))}`);
 
-    console.log(`${chalk.bgMagenta(chalk.cyanBright("  NANO REACT APP  "))}`);
+  const args = require("yargs").argv;
+  const projectLocation = args._[0];
+  const projectType = args.ts ? "ts" : "js";
 
-    const args = require('yargs').argv;
-    const projectLocation = args._[0];
-    const projectType = args.tsx? 'tsx': 'jsx';
+  await funcs.validateParams(projectLocation);
+  const { projectPath, projectName } = await funcs.processParams(
+    projectLocation,
+  );
+  const templateLocation = projectType === "ts" ? templates.ts : templates.js;
 
-    await funcs.validateParams(projectLocation);
-    const { projectPath, projectName } = await funcs.processParams(projectLocation);
-    const templateLocation = projectType === 'tsx'? tsxPath: jsxPath;
-
-    await funcs.createFolder(projectPath);
-    await funcs.downloadTemplate(templateLocation, projectPath);
-    await funcs.updateProjectFiles(projectPath, projectName);
-    await funcs.notifyUser(projectPath, projectName);
+  await funcs.createFolder(projectPath);
+  await funcs.downloadTemplate(templateLocation, projectPath);
+  await funcs.updateProjectFiles(projectPath, projectName);
+  await funcs.notifyUser(projectPath, projectName);
 };
 
 main();
